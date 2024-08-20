0144095517c714b938cf2cd7b87c54ae9ae71cf3
Carlo Tolla <cetoli@gmail.com>
2024-08-20 15:44:43 -0300

    Create main.py, spike read from git (21).

---
+++ pyjr/carlo/main.py
@@ -0,0 +1,12 @@
+# ada.aventura.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Agentes da E.S.C.O.L.A.
+
+.. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
+
+Changelog
+---------
+
+.. versionadded::    24.08
+   |br| spike read from git (21).
+"""
